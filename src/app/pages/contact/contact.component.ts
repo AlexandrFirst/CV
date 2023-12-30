df6fc7dfe0d868a0cdff5835823bb835beb0cf13
Alexandr Logvinov <ato31ato@gmail.com>
2023-12-30 21:05:55 +0200

    small fixes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import VanillaTilt from "vanilla-tilt";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  constructor(private el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    VanillaTilt.init(
+      this.el.nativeElement.querySelectorAll(".contact-pic"), { max: 20, speed: 300, scale: 1.05 }
+    );
+  }
 }
